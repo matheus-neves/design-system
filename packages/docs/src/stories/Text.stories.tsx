2ef26ebd1339e01a5c8caf71b8dcdfcc5488a361
Matheus Neves <matheusneves1989@gmail.com>
2023-04-21 21:55:57 -0300

    feat(packages/docs): text doc

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta reprehenderit porro aliquam, soluta, laborum laboriosam est dolores vero, provident quibusdam voluptatem molestias? Veritatis, eos eveniet! Dolore expedita minus quo a?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
