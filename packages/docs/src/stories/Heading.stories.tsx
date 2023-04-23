a4aa522cd4cd9c80261300085b7803c9cadd6d85
Matheus Neves <matheusneves1989@gmail.com>
2023-04-23 15:23:18 -0300

    feat(packages/docs): heading stories

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default the heading will always be a H2, but we can change this with the `as` property',
+      },
+    },
+  },
+}
