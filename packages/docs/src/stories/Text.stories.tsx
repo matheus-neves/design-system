218d44cd781251339b61a81a8c50689bdd1c5146
Matheus Neves <matheusneves1989@gmail.com>
2023-05-05 22:08:30 -0300

    feat(packages/docs): set stories controls

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta reprehenderit porro aliquam, soluta, laborum laboriosam est dolores vero, provident quibusdam voluptatem molestias? Veritatis, eos eveniet! Dolore expedita minus quo a?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
