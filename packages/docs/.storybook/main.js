e88cc5ee12bf3798996e507dbebec9befb3082af
Matheus Neves <matheusneves1989@gmail.com>
2023-05-18 19:50:08 -0300

    fix: rollback storybook version

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,30 +1,23 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
-  addons: [
-    "@storybook/addon-links", 
-    "@storybook/addon-essentials", 
-    "@storybook/addon-interactions", 
-    "@storybook/addon-mdx-gfm", 
+module.exports = {
+  "stories": ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  framework: {
-    name: '@storybook/react-vite'
-  },
-  core: {
+  "framework": "@storybook/react",
+  "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
     "storyStoreV7": true
   },
-  docs: {
-    autodocs: "tag"
-  },
-  viteFinal: (config, {configType}) => {
-    if(configType === 'PRODUCTION') {
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
       config.base = '/design-system'
     }
+
     return config
   }
-};
-export default config;
+}
